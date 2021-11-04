e088bcedf3bd577b3d22da6b83bfa0f608f9715d
Reka Forgacs <rekaforgacs@Rekas-MBP.broadband>
2021-11-04 12:38:19 +0000

    start on junit tests

---
+++ test/Dec2HexTest.java
@@ -0,0 +1,44 @@
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/** 
+* Dec2Hex Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Nov 3, 2021</pre> 
+* @version 1.0 
+*/ 
+public class Dec2HexTest {
+
+    Dec2Hex dec2hex;
+
+@Before
+public void before() throws Exception {
+    dec2hex = new Dec2Hex();
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: main(String args[]) 
+* 
+*/ 
+    @Test
+     public void testWithCorrectInput(){
+    }
+
+    @Test
+    public void testNoInput(){
+    }
+
+    @Test
+    public void testWithNonIntegerInput(){
+    }
+}

84cfc5085d13e6e01442e5a6316896608abfb899
Reka <reka.forgacs@lloydsbanking.com>
2021-11-05 10:46:08 +0000

    updates

--- src/Dec2Hex.java
+++ src/Dec2Hex.java
@@ -3,19 +3,26 @@ class Dec2Hex
 
     public static int Arg1;
 
-    public static void main(String[] args)
+    public static String main(String[] args)
     {
+        //   Dec2Hex dec2Hex = new Dec2Hex();
+        //  dec2Hex.fetchNumber()
+
+        // validation check
         if(args.length <= 0){
             System.err.println("Please provide a number to convert.");
             System.exit(0);
         }
 
+        // parsing int
         try {
             Arg1 = Integer.parseInt(args[0]);
+
         } catch (NumberFormatException e){
             System.err.println("Please provide a valid number");
             System.exit(0);
         }
+
 
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
@@ -23,6 +30,7 @@ class Dec2Hex
         String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
+        // convertion
         while(num != 0)
         {
             rem=num%16;
@@ -31,6 +39,6 @@ class Dec2Hex
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
-
+        return hexadecimal;
+        // probably want to return something to assert
     }
-}
--- test/Dec2HexTest.java
+++ test/Dec2HexTest.java
@@ -1,44 +1,51 @@
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.Before; 
+import org.junit.Before;
 import org.junit.After;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-/** 
-* Dec2Hex Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>Nov 3, 2021</pre> 
-* @version 1.0 
-*/ 
+/**
+ * Dec2Hex Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>Nov 3, 2021</pre>
+ */
 public class Dec2HexTest {
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
-    Dec2Hex dec2hex;
+    //Dec2Hex dec2hex;
 
-@Before
-public void before() throws Exception {
-    dec2hex = new Dec2Hex();
-} 
+    @Before
+    public void before() throws Exception {
+        //dec2hex = new Dec2Hex();
+    }
 
-@After
-public void after() throws Exception { 
-} 
+    @After
+    public void after() throws Exception {
+    }
 
-/** 
-* 
-* Method: main(String args[]) 
-* 
-*/ 
+    /**
+     * Method: main(String args[])
+     */
     @Test
-     public void testWithCorrectInput(){
+    public void testWithCorrectInput() {
+        Dec2Hex.main(new String[] {"100"});
     }
 
     @Test
-    public void testNoInput(){
+    public void testNoInput() {
+        exit.expectSystemExit();
+        Dec2Hex.main(new String[]{});
     }
 
     @Test
-    public void testWithNonIntegerInput(){
+    public void testWithNonIntegerInput() {
+        exit.expectSystemExit();
+        Dec2Hex.main(new String[] {"sometext"});
     }
 }
